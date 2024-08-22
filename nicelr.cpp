499a66b199c0caab729a7912a8324c9c75709adb
Tomasz Woźniak <wozniak.tom@pm.me>
2024-08-22 20:55:31 +1000

    Update nicelr.cpp

--- nicelr.cpp
+++ nicelr.cpp
@@ -1,11 +1,18 @@
 #include <RcppArmadillo.h>
 // [[Rcpp::depends(RcppArmadillo)]]
 using namespace arma;
+using namespace Rcpp;
 
 // [[Rcpp::export]]
-vec nicelr (vec y, mat x) {
+List nicelr (vec y, mat x) {
   vec beta_hat = solve(x.t() * x, x.t() * y);
-  return beta_hat;
+  double sigma2_hat = as_scalar( (y - x * beta_hat).t() * (y - x * beta_hat) / y.n_elem );
+  mat cov_beta_hat = sigma2_hat * inv_sympd(x.t() * x);
+  return List::create(
+    _["beta_hat"] = beta_hat, 
+    _["sigma2_hat"] = sigma2_hat, 
+    _["cov_beta_hat"] = cov_beta_hat
+  );
 }
 
 /*** R
